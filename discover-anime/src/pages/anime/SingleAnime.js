2e17ece17270e454f921fc66d30d2449b3f523da
John Almazan <almazankaze@live.com>
2022-10-14 00:35:14 -0500

    continued on anime page with a starter banner

--- discover-anime/src/pages/anime/SingleAnime.js
+++ discover-anime/src/pages/anime/SingleAnime.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getSingleAnime } from "../../actions/anime";
 import Loader from "../../components/loader/Loader";
+import ImageBanner from "../../components/imageBanner/ImageBanner";
 
 function SingleAnime() {
   const { id } = useParams();
@@ -21,7 +22,54 @@ function SingleAnime() {
       <Loader />
     </div>;
   }
-  return <div className="anime-page app-container">Hello</div>;
+
+  if (animeState.anime.length <= 0) return null;
+
+  return (
+    <div className="anime-page app-container">
+      <ImageBanner
+        image={animeState.anime.attributes.coverImage.large}
+        title={animeState.anime.attributes.canonicalTitle}
+        description={animeState.anime.attributes.synopsis}
+      />
+
+      <div className="anime-info-container">
+        <img
+          src={animeState.anime.attributes.posterImage.small}
+          alt="anime poster"
+        />
+        <div className="anime-info">
+          <p>
+            <span className="info-data">title :</span> name
+          </p>
+          <p>
+            <span className="info-data">other name :</span> otherName
+          </p>
+          <p>
+            <span className="info-data">jp name :</span> jpName
+          </p>
+          <p>
+            <span className="info-data">type :</span> type
+          </p>
+          <p>
+            <span className="info-data">episodes :</span> epCount
+          </p>
+          <p>
+            <span className="info-data">aired :</span> date
+          </p>
+          <p>
+            <span className="info-data">rating :</span> rating
+          </p>
+          <p>
+            <span className="info-data">score :</span> score
+          </p>
+          <p>
+            <span className="info-data">popularity rank :</span> rank
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SingleAnime;
